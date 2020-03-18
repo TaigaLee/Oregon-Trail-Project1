be43173673bb304c5a14dbeaf4fbc5e18f713333
Taiga Lee <taiga@taigalee.com>
2020-03-18 13:35:45 -0500

    basic html and css of main game screen

--- js/app.js
+++ js/app.js
@@ -17,5 +17,33 @@ const game = {
   distance: 0,
   wagon: {
     health: 100
+  },
+  startGame: function() {
+    new Pioneer();
+    const $gameScreen = $(".game-screen");
+    const $stats = $(".stats-bar");
+    const $walkButtons = $(".speed-buttons");
+    const $dayTracker = $("<h2 id='days'>Day: " + this.days + "</h2>");
+    const $foodTracker = $("<h2 id='food'>Food: " + this.food + "</h2>");
+    const $moneyTracker = $("<h2 id='money'>Money: " + this.money + "</h2>");
+    const $distanceTracker = $(
+      "<h2 id='distance'>Distance Travelled: " + this.distance + "</h2>"
+    );
+    $stats.append($dayTracker);
+    $stats.append($distanceTracker);
+    $stats.append($foodTracker);
+    $stats.append($moneyTracker);
+    $(".main-game-image").append(
+      $("<img id='main-game-image' src=https://i.imgur.com/XLTUCpX.png>")
+    );
+    $walkButtons.append($("<button id='run-button'>Run</button>"));
+    $walkButtons.append($("<button id='walk-button'>Walk</button>"));
+    $walkButtons.append($("<button id='stroll-button'>Stroll</button>"));
+    $walkButtons.append($("<button id='stop-button'>Stop</button>"));
   }
 };
+
+$("#start-button").on("click", function() {
+  $(".welcome-screen").remove();
+  game.startGame();
+});
